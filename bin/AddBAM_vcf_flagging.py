26c0d634e767f9fb8946ea5c101203c355793f39
ab63 <ab63@sanger.ac.uk>
2023-01-11 15:38:03 +0000

    cleanup

--- bin/AddBAM_vcf_flagging.py
+++ bin/AddBAM_vcf_flagging.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
+##################################################
+# Copyright (c) 2023 Genome Research Ltd.
+
+# Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
+
+
+# This file is part of hairpin-wrapper.
+
+
+# hairpin-wrapper is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+##################################################
+
 # annotate vcf with output of AdditionalBAMStatistics
-# I am not responsible for this if statement, it is inherited
 from __future__ import annotations
 import sys
 if not sys.version_info >= (3, 10):
